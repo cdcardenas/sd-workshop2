5102f2d4a6135d7d98b29cc0d409de1447baabc1
Christian Cardenas <crisdavcar@gmail.com>
2018-02-19 20:16:00 -0500

    added sensu-client

--- A00212740/rabbit-conf.sh
+++ A00212740/rabbit-conf.sh
@@ -7,3 +7,6 @@ chown -R rabbitmq:rabbitmq /var/lib/rabbitmq/
 rabbitmqctl add_user mqadmin mqadminpassword
 rabbitmqctl set_user_tags mqadmin administrator
 rabbitmqctl set_permissions -p / mqadmin ".*" ".*" ".*"
+rabbitmqctl add_vhost /sensu
+rabbitmqctl add_user sensu password
+rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
---
+++ A00212740/sensu-client/rabbit-conf.sh
@@ -0,0 +1,19 @@
+	
+
+#systemctl start rabbitmq-server.service
+#systemctl enable rabbitmq-server.service
+#rabbitmq-plugins enable rabbitmq_management
+#chown -R rabbitmq:rabbitmq /var/lib/rabbitmq/
+#rabbitmqctl add_user mqadmin mqadminpassword
+#rabbitmqctl set_user_tags mqadmin administrator
+#rabbitmqctl set_permissions -p / mqadmin ".*" ".*" ".*"
+#rabbitmqctl add_vhost /sensu
+#rabbitmqctl add_user sensu password
+#rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
+
+cd /vagrant
+cp -r sensu/ /etc/
+
+yum install screen -y
+yum install httpd -y
+service sensu-client start
